0ddb02ff81f0b51b4081be8dfd73bc0e6f657708
Crevista <billybickell@hotmail.co.uk>
2025-04-26 14:30:16 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Crevista</h1>
+      <p>Helping TikTok Shop Affiliates and UGC Creators grow faster.</p>
+      <p>Cliprr is coming soon...</p>
+    </div>
+  );
+}
